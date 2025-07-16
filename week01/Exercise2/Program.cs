940726a2483183eca4c072d275c0e546db0b4020
Yanny Arellano <yanni.arellano23@gmail.com>
2025-07-15 21:29:05 -0500

    Grades if statment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Helloo World! This is the Exercise2 Project.");
+        /*Write a program that determines the letter grade for a course according to the following scale:
+            A >= 90
+            B >= 80
+            C >= 70
+            D >= 60
+            F < 60*/
+
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        gradePercentage = gradePercentage.Replace("%", "");
+
+        int grade = int.Parse(gradePercentage);
+
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = grade % 10;
+        if (letter != "F")
+        {
+            if (letter == "A" && lastDigit >= 7)
+            {
+                sign = "";
+            }
+            else if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit <= 3)
+            {
+                sign = "-";
+            }
+        }
+        Console.WriteLine($"Your grade is {letter}{sign}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats, You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You not passed, good luck next time");
+        }
     }
 }
+
+
